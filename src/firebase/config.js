abe73f3f152c3346f1c9af78ead9693d06b77c85
Chai Phonbopit <phonbopit@gmail.com>
2020-03-28 00:30:33 +0700

    Add example

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA54JF5uiYCEz3A8Zi_XHKD5DwauZMGWrU',
+  authDomain: 'hello-example-react-firebase.firebaseapp.com',
+  databaseURL: 'https://hello-example-react-firebase.firebaseio.com',
+  projectId: 'hello-example-react-firebase',
+  storageBucket: 'hello-example-react-firebase.appspot.com',
+  messagingSenderId: '46372697830',
+  appId: '1:46372697830:web:c18e283854da2335de2d27'
+};
+
+export default firebaseConfig;
